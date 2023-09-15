5da0a0f1cd217d2dfc4b9499aad111668d43779a
tartom7997 <tartom7997@gmail.com>
2023-09-15 17:22:36 +0900

    update LP text and image

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -9,11 +9,13 @@ export default function Home() {
           <div>
             <span className="bg-black text-white p-2 rounded">Beta</span>
             <h1 className="text-3xl font-bold mt-4">
-              Unlock the Power of Web3 Referrals with AI & Tokens
+              Embark on the referral hiring of early crypto startups
             </h1>
             <p className="mt-4">
-              Boost referrals with AI, reward with tokens, and celebrate with
-              recommendation NFTs. Elevate your referral hiring today.
+              Harness the boundless network of Web3 social.
+            </p>
+            <p className="mt-4">
+              Request referrals with rewards to friends and even beyond, and discover the qualified talents to invite to your team.
             </p>
             <div className="mt-4">
               <a href="#waitlistSection">
@@ -35,10 +37,12 @@ export default function Home() {
 
         <div className="grid gap-8 grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2">
           <div className="order-1 xl:order-2">
-            <h2 className="text-2xl font-bold">Make connections meaningful</h2>
+            <h2 className="text-2xl font-bold">Ask Web3 Frens for Referrals</h2>
             <p className="mt-4">
-              Utilize web3 social connections like Farcaster and Lens to extract
-              friends whom we should ask for referrals.
+              Leverage Web3 social like Farcaster and Lens to request referrals from crypto specialists and their networks.
+            </p>
+            <p className="mt-4">
+              Offer reward incentives to broaden your reach to talents, filtering candidates based on location, tags, and interests.
             </p>
           </div>
           <div className="flex justify-center items-center min-h-[300px] order-2 xl:order-1">
@@ -55,8 +59,13 @@ export default function Home() {
           <div className="order-1">
             <h2 className="text-2xl font-bold">AI-Powered Personalization</h2>
             <p className="mt-4">
-              Our AI crafts personalized direct messages tailored for each
-              referral request without taking time and request limits.
+              Capture hearts with tailored messages for each individual.
+            </p>
+            <p className="mt-4">
+              Let AI craft the perfect DM for you, and instantly provide qualified recommendations incorporating the voice of the referrer.
+            </p>
+            <p className="mt-4">
+              Without taking time and request limits, ask many friends for help.
             </p>
           </div>
           <div className="flex justify-center items-center min-h-[300px] order-2">
@@ -75,9 +84,13 @@ export default function Home() {
               Tokenized Rewards & Recommendations
             </h2>
             <p className="mt-4">
-              Incentivize referrals with rewards and create NFT recommendations
-              comparing requirements by AI, amplifying reach, and qualifying
-              talents.
+              Referrers can further request referrals from their network and split the rewards within.
+            </p>
+            <p className="mt-4">
+              With AI, compare job requirements with recommendations, creating a vetted recommendation that assesses the candidate&rsquo;s suitability.
+            </p>
+            <p className="mt-4">
+              This recommendation then becomes an NFT, a prestigious item shaping the reputation of the referred individual.
             </p>
           </div>
           <div className="flex justify-center items-center min-h-[300px] order-2 xl:order-1">
